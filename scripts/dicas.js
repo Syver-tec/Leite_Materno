8179753a7cc849be92ba9f15bc39fd030c03c89f
Jose Lima <joselimaprofissional18@gmail.com>
2026-03-31 23:06:54 -0300

    Adicionado Dicas. Alterado home e demais páginas

---
+++ scripts/dicas.js
@@ -0,0 +1,19 @@
+// Seleciona todos os headers do accordion
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    
+    const item = header.parentElement;
+
+    // Fecha todos (comportamento clássico de accordion)
+    document.querySelectorAll(".accordion-item").forEach(i => {
+      if (i !== item) {
+        i.classList.remove("active");
+      }
+    });
+
+    // Alterna o atual
+    item.classList.toggle("active");
+  });
+});
